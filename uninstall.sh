f164057d758d61b0bf0ce6fc788ef24a1eebe469
Greg Saltis <gregorysaltis@hotmail.com>
2020-11-03 20:37:40 -0500

    Add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+rm -rf /etc/init.d/canmon.rc
+rm -rf /etc/rc1.d/S99canmon
+rm -rf /etc/rc2.d/S99canmon
+rm -rf /etc/rc3.d/S99canmon
+rm -rf /etc/rc4.d/S99canmon
+rm -rf /etc/rc5.d/K99canmon
+rm -rf /etc/rc6.d/K99canmon
